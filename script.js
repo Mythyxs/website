465616c9132282a18daa809ea0aff796579191fc
Mythyxs <shemxz123@gmail.com>
2025-06-03 13:41:54 -0400

    redesign

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("loaded");
+const phrases = ["Web Developer", "Gamer", "Anime Enthusiast", "Designer"];
+let index = 0;
+let charIndex = 0;
+const typingEl = document.getElementById("typing");
+
+function type() {
+  if (charIndex <= phrases[index].length) {
+    typingEl.innerText = phrases[index].slice(0, charIndex++);
+    setTimeout(type, 100);
+  } else {
+    setTimeout(() => {
+      charIndex = 0;
+      index = (index + 1) % phrases.length;
+      type();
+    }, 1500);
+  }
+}
+
+function glowButtonClick() {
+  alert("More coming soon...");
+}
+
+type();
