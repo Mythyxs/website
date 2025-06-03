ed2adfceeed5a0a6fda88a594c807894ff900d69
Mythyxs <shemxz123@gmail.com>
2025-06-03 13:59:25 -0400

    accolades

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-const phrases = ["Web Developer", "Gamer", "Anime Enthusiast", "Designer"];
+const phrases = ["C++", "Python", "Batch", "Lua"];
+const shuffled = phrases.sort(() => Math.random() - 0.5);
+
 let index = 0;
 let charIndex = 0;
 const typingEl = document.getElementById("typing");
 
 function type() {
-  if (charIndex <= phrases[index].length) {
-    typingEl.innerText = phrases[index].slice(0, charIndex++);
+  if (charIndex <= shuffled[index].length) {
+    typingEl.innerText = shuffled[index].slice(0, charIndex++);
     setTimeout(type, 100);
   } else {
     setTimeout(() => {
       charIndex = 0;
-      index = (index + 1) % phrases.length;
+      index = (index + 1) % shuffled.length;
       type();
     }, 1500);
   }
 }
 
 function glowButtonClick() {
-  alert("More coming soon...");
+  document.getElementById("accolades").scrollIntoView({ behavior: "smooth" });
 }
 
 type();
